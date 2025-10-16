4652978e81606404e55ff8b0164585b64d9adceb
Singh-Arshvir <79410121+Singh-Arshvir@users.noreply.github.com>
2025-10-16 18:20:17 +0530

    cors problem 3 passing as string

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
   {
-    origin: "process.env.CLIENT_URL",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
     credentials: true
   }
